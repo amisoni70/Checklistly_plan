f23197c24c32a1815241631c2013b6a1383faf19
amisoni70 <amisoni7014@gmail.com>
2024-05-23 18:55:54 +0300

    TodoList Compenent- making the input field and button functional

--- checklistly_plan/src/TodoList/TodoList.js
+++ checklistly_plan/src/TodoList/TodoList.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function TodoList({ user }) {
+
+const [todo, setTodo] = useState("");
+
   return (
     <div>
       <h2>{user}'s Todo List</h2>
-      {/* Todo list implementation */}
+      {/* Adding a Task in the input field */}
+      <input
+      type="text"
+      value={todo}
+      onChange={(e) => setTodo(e.target.value)}
+      />
+      <button>Add task</button>
     </div>
   );
 }
