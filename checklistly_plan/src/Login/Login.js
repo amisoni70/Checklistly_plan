22fc59a87f1a453d2f874d7046f8de50834f6899
amisoni70 <amisoni7014@gmail.com>
2024-05-28 21:54:52 +0300

    Improved the Login- password input functionality

--- checklistly_plan/src/Login/Login.js
+++ checklistly_plan/src/Login/Login.js
@@ -3,9 +3,10 @@ import "./Login.css";
 
 //Login component takes a prop called isLoggedIn from a parent component
 function Login({isLoggedIn}) {
-    const [email, setEmail] = useState('');   //defines the email variable
-    const [password, setPassword] = useState('');   //defines the password variable
-    const [emailError, setEmailError] = useState(''); //defines the error in the email input 
+    const [email, setEmail] = useState('');   // stores values for the email
+    const [password, setPassword] = useState('');   //stores values for the password
+    const [emailError, setEmailError] = useState(''); //stores values for the email errors
+    const [passwordError, setPasswordError ] = useState(''); // stores values for the password errors
 
 // Event handler functions
 const handleLogin = () => {
@@ -26,6 +27,18 @@ const handleEmailChange = (e) => {
 const isValidEmail = () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   return emailRegex.test(email);
+};
+
+const handlePasswordChange = (e) => {
+  const inputPassword = e.target.value;
+  if (inputPassword.length <= 8) {
+      setPassword(inputPassword);
+      if (inputPassword.length < 8) {
+          setPasswordError('Password must be exactly 8 characters');
+      } else {
+          setPasswordError('');
+      }
+  }
 };
 
 return (
@@ -51,8 +64,9 @@ return (
           type="password" 
           placeholder="Password" 
           value={password} 
-          onChange={(e) => setPassword(e.target.value)} 
+          onChange={handlePasswordChange} 
         />
+        {passwordError && <p style={{ color: 'red', fontWeight: 'bold' }}>{passwordError}</p>}
         <button onClick={handleLogin}>LOGIN</button>
       </div>
     </div>
